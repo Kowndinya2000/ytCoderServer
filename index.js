ea369e4146aa0f041996084a46793d91efb35063
Kowndinya2000 <cs17b032@iittp.ac.in>
2020-05-15 23:25:18 +0530

    cors fix 1

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ express()
   .use(cors({
       credentials: true,
   }))
+  .use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+    })
   .get('/', (req, res) => {
     res.header('Acess-Control-Allow-Credentials','true')
     res.header('Access-Control-Allow-Origin', '*');
